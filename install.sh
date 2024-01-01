006473eaeee638708bab02f8bf41b53ea174273f
Vinod <vinod@debian.lan>
2024-01-01 13:34:30 -0600

    Install Neovim

--- install.sh
+++ install.sh
@@ -1,2 +1,18 @@
 # Install Alacritty
 sudo apt-get install alacritty
+
+# Install Neovim
+# Prerequisites
+sudo apt-get install ninja-build gettext cmake unzip curl
+echo "Moving to home folder"
+cd ~
+echo "Checking for Neovim Folder ..."
+rm -rf Neovim
+echo "Removing installed version of Neovim..."
+apt-get remove neovim
+# Download Neovim
+git clone https://github.com/neovim/neovim
+cd neovim && make CMAKE_BUILD_TYPE=Release
+git checkout stable
+# Using this we can uninstall installed version of Neovim
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
