ec8cdaf747990f98179a8cef61b158aa3b8fc499
Vinod Nair <vinod.nair@omshealth.com>
2024-01-21 22:31:08 -0600

    Started init.lua

---
+++ neovim/nvim/init.lua
@@ -0,0 +1,110 @@
+-- init.lua
+-- Vinod Nair MD
+-- https://www.youtube.com/watch?v=zHTeCSVAFNY&t=30s
+
+
+vim.g.mapleader = ','
+vim.g.localleader = ';'
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+
+
+
+-- lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = { 
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }},
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'}
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+
+-- Telescope setup
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- Treesetter 
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_enabled = {"c", "lua", "python"},
+  sync_install   = false,
+  highlight = {enable = true},
+  indent = {enable = true},
+})
+
+
+-- Color scheme
+require("catppuccin").setup({
+  flavour = "macchiato", -- latte, frappe, macchiato, mocha
+  background = { -- :h background
+  light = "latte",
+  dark = "mocha",
+},
+transparent_background = false, -- disables setting the background color.
+show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+dim_inactive = {
+  enabled = false, -- dims the background color of inactive window
+  shade = "dark",
+  percentage = 0.15, -- percentage of the shade to apply to the inactive window
+},
+no_italic = false, -- Force no italic
+no_bold = false, -- Force no bold
+no_underline = false, -- Force no underline
+styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+comments = { "italic" }, -- Change the style of comments
+conditionals = { "italic" },
+loops = {},
+functions = {},
+keywords = {},
+strings = {},
+variables = {},
+numbers = {},
+booleans = {},
+properties = {},
+types = {},
+operators = {},
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      treesitter = true,
+      notify = false,
+      mini = {
+        enabled = true,
+        indentscope_color = "",
+      },
+      -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+  })
+
+  -- setup must be called before loading
+  vim.cmd.colorscheme "catppuccin"
+
+
