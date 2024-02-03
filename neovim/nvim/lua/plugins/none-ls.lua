414ea6f18d981f70d6074038b1aace24953e34f5
Vinod Nair <vinod.nair@omshealth.com>
2024-02-03 17:52:13 -0600

    Added toggleterm

---
+++ neovim/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,18 @@
+-- none-ls.lua
+
+return {
+
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.clang_format.lua,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
+}
---
+++ neovim/nvim/lua/plugins/toggle-term.lua
@@ -0,0 +1,15 @@
+-- toggleterm.lua
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			direction = "float",
+			open_mapping = "<C-t>",
+			auto_scroll = true,
+			float_opts = {
+				border = "single",
+			},
+		})
+	end,
+}
