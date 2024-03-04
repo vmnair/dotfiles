353e23beb28149489996aebb6bc4a55859b86fce
Vinod Nair <drvinodnair@gmail.com>
2024-03-04 00:21:33 -0600

    Added lazy-setup bootstrap code into a seperate file.

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -1,25 +1,7 @@
 -- Vinod Nair MD
 -- https://www.youtube.com/watch?v=zHTeCSVAFNY&t=30s
 
-
-
--- Lazy.nvim boot-strapping
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 require("vim-options")
-require("lazy").setup("plugins")
-
-
+require("lazy-setup")
 
 
--- neovim/nvim/lua/plugins/catppuccin.lua
+++ neovim/nvim/lua/plugins/catppuccin.lua
@@ -3,15 +3,16 @@
 
 
 return { "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme catppuccin]])
-  end,
-  opts = {
-    flavor = "macchiato", -- latte, frappem macchiato, mocha
-  }
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        -- vim.cmd([[colorscheme catppuccin]])
+        vim.cmd.colorscheme("catppuccin")
+    end,
+    opts = {
+        flavor = "macchiato", -- latte, frappem macchiato, mocha
+    }
 
 }
 
