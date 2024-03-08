defd86d524396e11c85b5c77f4e8983dc23c21c1
Vinod Nair <vinod.nair@omshealth.com>
2024-03-08 11:56:56 -0600

    updated catppuccin

--- neovim/nvim/lua/plugins/catppuccin.lua
+++ neovim/nvim/lua/plugins/catppuccin.lua
@@ -2,12 +2,12 @@
 
 
 
-return { "catppuccin/nvim", 
+return { "catppuccin/nvim",
   lazy = false,
-  name = "catppuccin", 
+  name = "catppuccin",
   priority = 1000,
   config = function()
-    vim.cmd([[colorscheme catppuccin]])
+    vim.cmd([[colorscheme catppuccin ]])
   end,
   opts = {
     flavor = "macchiato", -- latte, frappem macchiato, mocha
