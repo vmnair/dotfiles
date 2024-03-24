4f0e35320ca574aa76175ac7254f935beee3b4f8
Vinod Nair <vinod.nair@omshealth.com>
2024-03-24 12:33:21 -0500

    Updated toggleterm

--- neovim/nvim/lua/plugins/toggle-term.lua
+++ neovim/nvim/lua/plugins/toggle-term.lua
@@ -4,8 +4,22 @@ return {
 	version = "*",
 	config = function()
 		require("toggleterm").setup({
-      open_mapping = "<leader>tt",
-      autochdir = true, -- Sync with Neovim directory
+			open_mapping = "<leader>tt",
+			insert_mapping = true, -- Mappings active in insert mode.
+			autochdir = true, -- Sync with Neovim directory
 		})
+
+
+
+		-- function to set keymaps
+		function _G.set_terminal_keymaps()
+			local opts = { noremap = true, silent = true }
+			local map = vim.api.nvim_buf_set_keymap
+			map(0, "t", "<esc>", [[<c-\><c-n>]], opts) -- escape -> normal mode
+		end
+
+		-- Set keymaps using the above function
+		vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 	end,
+
 }
