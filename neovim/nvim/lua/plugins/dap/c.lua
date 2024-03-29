14ca23ba1223da6313bb126bcef5c71cf088da73
Vinod Nair <drvinodnair@gmail.com>
2024-03-28 21:05:45 -0500

    Updated dap/c.lua to reflect vscode-lldb-14 on linux systems

--- neovim/nvim/lua/plugins/dap/c.lua
+++ neovim/nvim/lua/plugins/dap/c.lua
@@ -6,32 +6,38 @@ if not ok then
 end
 
 -- Function to check operating system
-function GetOS()
-	if os.getenv("OS") ~= nil and os.getenv("OS"):match("Windows") then
-		return "Windows"
-	elseif os.getenv("HOME") ~= nil then
-		if os.getenv("XDG_CURRENT_DESKTOP") ~= nil then
-			return "Linux"
-		else
-			return "macOS"
-		end
-	else
-		return "Unknown"
-	end
-end
+ function GetOS()
+ 	if os.getenv("OS") ~= nil and os.getenv("OS"):match("Windows") then
+ 		return "Windows"
+ 	elseif os.getenv("HOME") ~= nil then
+ 		if os.getenv("XDG_CURRENT_DESKTOP") ~= nil then
+ 			return "Linux"
+ 		else
+ 			return "macOS"
+ 		end
+ 	else
+         error("Unknown operating system", 1)
+ 		return "Unknown"
+ 	end
+ end
 
-local os = GetOS()
-local path_to_vscode
-if os == "macOS" then
-	path_to_vscode = "/opt/homebrew/opt/llvm/bin/lldb-vscode"
-elseif os == "Linux" then
-    path_to_vscode = "/usr/bin/lldb"
-end
+
+ local system = GetOS()
+ print("OS = " .. system)
+
+ local path_to_lldb
+ if system == "macOS" then
+ 	path_to_lldb = "/opt/homebrew/opt/llvm/bin/lldb-vscode"
+ elseif system  == "Linux" then
+     path_to_lldb = "lldb-vscode-14" --lldb-vscode-14
+ else
+     error("Unknown operating system: " .. system)
+ end
 
 dap.adapters.lldb = {
 	name = "lldb",
 	type = "executable",
-	command = path_to_vscode,
+	command = path_to_lldb
 
 }
 
