19b3d1eaf8b27cd11a23f30fd1b08008d6792223
Vinod Nair <vinod.nair@omshealth.com>
2024-04-12 22:40:29 -0500

    Added functionality to compile and run C CMake Project

--- neovim/nvim/lua/plugins/treesitter.lua
+++ neovim/nvim/lua/plugins/treesitter.lua
@@ -1,74 +1,74 @@
 -- treesitter.lua
 
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        -- Treesetter
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_enabled = { "c", "lua", "vim", "vimdoc", "query" },
-            -- automatically install parser if a new filetype has none.
-            auto_install = true,
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<Leader>ss",
-                    node_incremental = "<Tab>",
-                    scope_incremental = "<S-Tab>",
-                    node_decremental = "<BS>",
-                },
-            },
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		-- Treesetter
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_enabled = { "c", "lua", "vim", "vimdoc", "query" },
+			-- automatically install parser if a new filetype has none.
+			auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<Leader>ss",
+					node_incremental = "<Tab>",
+					scope_incremental = "<S-Tab>",
+					node_decremental = "<BS>",
+				},
+			},
 
-            textobjects = {
-                select = {
-                    enable = true,
+			textobjects = {
+				select = {
+					enable = true,
 
-                    -- Automatically jump forward to textobj, similar to targets.vim
-                    lookahead = true,
+					-- Automatically jump forward to textobj, similar to targets.vim
+					lookahead = true,
 
-                    keymaps = {
-                        -- You can use the capture groups defined in textobjects.scm
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        -- You can optionally set descriptions to the mappings (used in the desc parameter of
-                        -- nvim_buf_set_keymap) which plugins like which-key display
-                        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-                        -- You can also use captures from other query groups like `locals.scm`
-                        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-                    },
-                    -- You can choose the select mode (default is charwise 'v')
-                    --
-                    -- Can also be a function which gets passed a table with the keys
-                    -- * query_string: eg '@function.inner'
-                    -- * method: eg 'v' or 'o'
-                    -- and should return the mode ('v', 'V', or '<c-v>') or a table
-                    -- mapping query_strings to modes.
-                    selection_modes = {
-                        ["@parameter.outer"] = "v", -- charwise
-                        ["@function.outer"] = "V", -- linewise
-                        ["@class.outer"] = "<c-v>", -- blockwise
-                    },
-                    -- If you set this to `true` (default is `false`) then any textobject is
-                    -- extended to include preceding or succeeding whitespace. Succeeding
-                    -- whitespace has priority in order to act similarly to eg the built-in
-                    -- `ap`.
-                    --
-                    -- Can also be a function which gets passed a table with the keys
-                    -- * query_string: eg '@function.inner'
-                    -- * selection_mode: eg 'v'
-                    -- and should return true or false
-                    include_surrounding_whitespace = true,
-                },
-            },
-        })
-    end,
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						-- You can optionally set descriptions to the mappings (used in the desc parameter of
+						-- nvim_buf_set_keymap) which plugins like which-key display
+						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+						-- You can also use captures from other query groups like `locals.scm`
+						["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+					},
+					-- You can choose the select mode (default is charwise 'v')
+					--
+					-- Can also be a function which gets passed a table with the keys
+					-- * query_string: eg '@function.inner'
+					-- * method: eg 'v' or 'o'
+					-- and should return the mode ('v', 'V', or '<c-v>') or a table
+					-- mapping query_strings to modes.
+					selection_modes = {
+						["@parameter.outer"] = "v", -- charwise
+						["@function.outer"] = "V", -- linewise
+						["@class.outer"] = "<c-v>", -- blockwise
+					},
+					-- If you set this to `true` (default is `false`) then any textobject is
+					-- extended to include preceding or succeeding whitespace. Succeeding
+					-- whitespace has priority in order to act similarly to eg the built-in
+					-- `ap`.
+					--
+					-- Can also be a function which gets passed a table with the keys
+					-- * query_string: eg '@function.inner'
+					-- * selection_mode: eg 'v'
+					-- and should return true or false
+					include_surrounding_whitespace = true,
+				},
+			},
+		})
+	end,
 
-    {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 }
--- neovim/nvim/lua/util.lua
+++ neovim/nvim/lua/util.lua
@@ -1,25 +1,45 @@
 -- util.lua
 -- Utility functions
 
-local util  = {}
-
+local util = {}
 -- Function to check operating system
--- Would return Windows, Linux, macOS or Unknown 
- function util.GetOS()
- 	if os.getenv("OS") ~= nil and os.getenv("OS"):match("Windows") then
- 		return "Windows"
- 	elseif os.getenv("HOME") ~= nil then
- 		if os.getenv("XDG_CURRENT_DESKTOP") ~= nil then
- 			return "Linux"
- 		else
- 			return "macOS"
- 		end
- 	else
-         error("Unknown operating system", 1)
- 		return "Unknown"
- 	end
- end
+-- Would return Windows, Linux, macOS or Unknown
 
+-- Check to see if a directory exists
+local function directory_exists(path)
+    -- try to open a directory as if it is a file.
+    local file = io.open(path, "r")
+    if file then
+        -- Attempt to read  from the directory, this will fail.
+        local block = file:read(1)
+        file:close()
+        if block == nil then
+            -- successfully opened, but unable to read (is directory
+            return true
+        else
+            -- could open and read, like a file, so not a directory
+            return false
+        end
+    else
+        -- Couldn't open or read
+        return false
+    end
+end
+
+function util.GetOS()
+    if os.getenv("OS") ~= nil and os.getenv("OS"):match("Windows") then
+        return "Windows"
+    elseif os.getenv("HOME") ~= nil then
+        if os.getenv("XDG_CURRENT_DESKTOP") ~= nil then
+            return "Linux"
+        else
+            return "macOS"
+        end
+    else
+        error("Unknown operating system", 1)
+        return "Unknown"
+    end
+end
 
 -- Zathura PDF Viewer
 -- Open Zathura
@@ -28,10 +48,85 @@ local function open_zathura(file_path)
     vim.cmd("!zathura " .. vim.fn.shellescape(file_path) .. " &")
 end
 
-
 vim.api.nvim_create_user_command("OpenZathura", function(input)
     open_zathura(input.args)
-end, {nargs = 1, desc = "Open Zathura"})
+end, { nargs = 1, desc = "Open Zathura" })
 
+-- C compilation
+-- Return the program name or nil
+local function compile_c_program_with_cmake()
+    -- See if CMakeLists.txt file & build/ directory exists
+    local match = nil
+    local file, err = io.open("CMakeLists.txt", "r")
+    if not file then
+        print("Error opening file: " .. err)
+        return
+    else
+        -- find the target name
+        local pattern = "add_executable%s*%((%S+)"
+        for line in file:lines() do
+            match = line:match(pattern)
+            if match then
+                break -- stop reading file.
+            end
+        end
+        file:close()
+    end
+    -- if so switch to build/directory
+    if not directory_exists("build") then
+        os.execute("mkdir build")
+    end
+    -- Change to the build directory and run CMake commands, silencing output
+    os.execute("cd build && cmake .. > /dev/null 2>&1 && cmake --build . > /dev/null 2>&1")
+    -- print("Compiled program: " .. match)
 
+    return match
+end
+
+local function compile_and_run_c_program_with_cmake()
+    local prog_name = compile_c_program_with_cmake()
+    return prog_name
+end
+
+vim.api.nvim_create_user_command("CMakeCompileCProgram", function()
+    local prog_name = compile_c_program_with_cmake()
+    print("Compiled -> " .. prog_name)
+end, { desc = "Compile C Program with CMake" })
+
+vim.api.nvim_create_user_command("CMakeCompileRunCProgram", function()
+    -- Get target name from CMakeLists.txt
+    local prog_name = compile_and_run_c_program_with_cmake()
+    --local cmd = "./build/" .. prog_name .. "; echo 'Press Enter to exit'; read"
+    local cmd = "./build/" .. prog_name
+
+    -- open a new buffer in horizontal split with 10 line of height
+    vim.cmd("10new")
+    -- get the buffer id of the new buffer
+    local buf_id = vim.api.nvim_get_current_buf()
+
+    -- Goto insert mode automatically
+    vim.api.nvim_buf_attach(buf_id, false, {
+        on_lines = function()
+            -- Check to see if we are in normal mode, if so switch to insert mode
+            if vim.api.nvim_get_mode().mode ~= "i" then
+                vim.cmd("startinsert")
+            end
+            return false -- detach the callback after switching to insert mode
+        end,
+    })
+
+    -- Start a terminal in the current buffer
+    vim.fn.termopen(cmd, {
+        on_exit = function(_, exit_code, _)
+            print("Program exited with code " .. exit_code)
+        end,
+    })
+
+    -- Start a terminal in the current buffer
+    --vim.fn.termopen(cmd)
+end, { desc = "Compile & run C Program with CMake" })
+
+-- Keybindings
+vim.keymap.set("n", "<leader>cr", "<cmd>CMakeCompileRunCProgram<cr>", { desc = "Compile + run a C program" })
+vim.keymap.set("n", "<leader>cc", "<cmd>CMakeCompileCProgram<cr>", { desc = "Compile + run a C program" })
 return util
