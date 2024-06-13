fb5d2ce30bd967ef9a2f228a73f6e823abd6abac
Vinod Nair <vinod.nair@omshealth.com>
2024-06-13 09:50:09 -0500

    Updated Oil.nvim configuration

--- neovim/nvim/lua/plugins/oil-nvim.lua
+++ neovim/nvim/lua/plugins/oil-nvim.lua
@@ -7,13 +7,21 @@ return {
     require("oil").setup{
         columns = { "icon"},
         default_file_explorer = true,
+        delete_to_trash = true, -- needs full disk access on mac
+        skip_confirm_for_simple_edits = true,
         keymaps = {
             ["<C-h"] = false,
             ["<M-h>"] = "actions.select.split",
         },
         view_options = {
             show_hidden = true,
+            is_always_hidden = function(name, _)
+                return name == '..' or name == '.git'
+            end,
         },
+        win_options = {
+            wrap = true,
+        }
     }
     --keymaps
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
