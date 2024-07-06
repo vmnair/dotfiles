a19b4a5d71845f9ed3b07b1d31128211d95f1760
Vinod Nair <vinod.nair@omshealth.com>
2024-07-06 13:18:16 -0500

    updated options.lua
    Based on Joseans article:
    https://www.josean.com/posts/how-to-setup-neovim-2024

--- neovim/nvim/lua/vinod/config/options.lua
+++ neovim/nvim/lua/vinod/config/options.lua
@@ -1,47 +1,51 @@
 -- vim-options.lua
+local opt = vim.opt -- Concisness
 
+opt.number = true
+opt.relativenumber = true
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+opt.splitright = true   --split horizondal window to below
+opt.splitbelow = true   -- split horizondal window to below
 
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+opt.wrap = false        -- disable line wraping
 
-vim.opt.wrap = false
+opt.expandtab = true
+opt.tabstop = 2         -- Expand tab to spaces
+opt.shiftwidth = 2
+opt.autoindent = true   -- Copy intent from the current line when starting new.
 
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-
--- Synchronizes the system clipboard with
--- Neovim's clipboard.
-vim.opt.clipboard= "unnamedplus"
+opt.clipboard= "unnamedplus"  -- Synchronizes the system clipboard
 
 -- Places cursor in the middle of the screen
-vim.opt.scrolloff = 999
+opt.scrolloff = 999
+opt.cursorline = true   -- Highlight current line.
 
-vim.opt.colorcolumn = "80"
-vim.opt.signcolumn = 'yes'
+opt.swapfile = false    -- Turn off swapfile.
 
-vim.opt.showmode = false
-vim.opt.showmatch = true
+
+opt.showmode = false
+opt.showmatch = true
 
 -- Ability to select cells where there are no
 -- characters, we will set it to only in 
 -- block mode.
-vim.opt.virtualedit = "block"
+opt.virtualedit = "block"
 
--- Show incremental search in a split
-vim.opt.inccommand = "split"
+opt.inccommand = "split"    -- Show incremental search in a split
+opt.ignorecase = true       -- Ignore case sensitivity in commands
+opt.smartcase  = true       -- Mixed case in search assumes case sensitivity.
 
--- Ignore case sensitivity in commands
-vim.opt.ignorecase = true
+opt.termguicolors = true
+opt.background = "dark"     -- Colorschemes (light or dark) will be made dark.
+opt.colorcolumn = "80"
+opt.signcolumn = 'yes'
 
-vim.opt.termguicolors = true
-vim.opt.encoding = "utf-8"
+opt.backspace = "indent,eol,start" -- Allow backspace on insert mode start.
+
+opt.encoding = "utf-8"
 
 -- Which-key
-vim.opt.timeout = true
-vim.opt.timeoutlen = 300
-vim.opt.mouse = ''
+opt.timeout = true
+opt.timeoutlen = 300
+opt.mouse = ''
 
