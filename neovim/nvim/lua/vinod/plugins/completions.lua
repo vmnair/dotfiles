db74927d4738eef457b7187761e9d177c3c081c6
Vinod Nair <vinod.nair@omshealth.com>
2024-08-11 21:48:00 -0500

    updated nvim-cmp

--- neovim/nvim/lua/vinod/plugins/completions.lua
+++ neovim/nvim/lua/vinod/plugins/completions.lua
@@ -1,83 +1,84 @@
 return {
   -- Completion sources
-	{
-		"hrsh7th/cmp-nvim-lsp", -- completion plugin for Neovim's build-in LSP
-		"hrsh7th/cmp-buffer", -- source for text in the current buffer
-		"hrsh7th/cmp-path", -- source for file system paths
-		"hrsh7th/cmp-cmdline", -- source for command line mode
-	},
+  {
+    "hrsh7th/cmp-nvim-lsp", -- completion plugin for Neovim's build-in LSP
+    "hrsh7th/cmp-buffer", -- source for text in the current buffer
+    "hrsh7th/cmp-path",   -- source for file system paths
+    "hrsh7th/cmp-cmdline", -- source for command line mode
+  },
   -- Snippet engine and dependencies
-	{
-		"L3MON4D3/LuaSnip", -- snippet engine
-		-- follow latest release.
-		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-		-- install jsregexp (optional!).
-		build = "make install_jsregexp",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip", -- source for integrating Luasnip with cmp
-			"rafamadriz/friendly-snippets", -- collection of predefined snippets
-		},
-	},
+  {
+    "L3MON4D3/LuaSnip", -- snippet engine
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip", -- source for integrating Luasnip with cmp
+      -- "rafamadriz/friendly-snippets", -- collection of predefined snippets
+    },
+  },
   -- nvim-cmp setup
-	{
-		"hrsh7th/nvim-cmp", -- Main completion engine.
-		config = function()
-			-- Set up nvim-cmp.
-			local cmp = require("cmp")
-			--require("luasnip.loaders.from_vscode").lazy_load()
+  {
+    "hrsh7th/nvim-cmp", -- Main completion engine.
+    config = function()
+      -- Set up nvim-cmp.
+      local cmp = require("cmp")
+      --require("luasnip.loaders.from_vscode").lazy_load()
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
 
-			-- Set configuration for specific filetype.
-			cmp.setup.filetype("gitcommit", {
-				sources = cmp.config.sources({
-					{ name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-				}, {
-					{ name = "buffer" },
-				}),
-			})
+      -- Set configuration for specific filetype.
+      --[[cmp.setup.filetype("gitcommit", {
+        sources = cmp.config.sources({
+          { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+        }, {
+          { name = "buffer" },
+        }),
+      }) ]]
+      --
 
-			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = "buffer" },
-				},
-			})
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
 
-			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "path" },
-				}, {
-					{ name = "cmdline" },
-				}),
-        matching = { disallow_symbol_nonprefix_matching = false }
-			})
-		end,
-	},
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false },
+      })
+    end,
+  },
 }
