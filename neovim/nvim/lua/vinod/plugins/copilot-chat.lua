a8e253c87b2be583487ecd0f72c65b95292ce5b8
Vinod Nair <vinod.nair@omshealth.com>
2024-08-24 18:55:27 -0500

    Added keymap for CopilotChat

--- neovim/nvim/lua/vinod/plugins/copilot-chat.lua
+++ neovim/nvim/lua/vinod/plugins/copilot-chat.lua
@@ -6,12 +6,17 @@ return {
 			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
 			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
 		},
+		build = "make tiktoken",
 		opts = {
-			debug = false, -- Enable debugging
+			debug = true, -- Enable debugging
 			question_header = "## Vinod ",
 			auto_follow_cursor = true,
 			highlight_selection = true,
 		},
 		cmd = { "CopilotChat" }, -- Lazy load
+		--{ "nmap", "<leader>cp", "<cmd>CopilotChat<cr>" },
+		--vim.api.nvim_set_keymap({ "n", "<leader>cp", "<Cmd>CopilotChat<CR>", {} }),
+
+		vim.api.nvim_set_keymap("n", "<leader>cp", "<cmd>CopilotChat<CR>", { noremap = true, silent = true }),
 	},
 }
