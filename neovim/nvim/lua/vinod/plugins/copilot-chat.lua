70814f8f2d1d00fa4c2141d0c5a46052a324e435
Vinod Nair <vinod.nair@omshealth.com>
2024-08-25 09:36:00 -0500

    updated CopilotChat code

--- neovim/nvim/lua/vinod/plugins/copilot-chat.lua
+++ neovim/nvim/lua/vinod/plugins/copilot-chat.lua
@@ -1,22 +1,27 @@
 return {
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "canary",
-		dependencies = {
-			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-		},
-		build = "make tiktoken",
-		opts = {
-			debug = true, -- Enable debugging
-			question_header = "## Vinod ",
-			auto_follow_cursor = true,
-			highlight_selection = true,
-		},
-		cmd = { "CopilotChat" }, -- Lazy load
-		--{ "nmap", "<leader>cp", "<cmd>CopilotChat<cr>" },
-		--vim.api.nvim_set_keymap({ "n", "<leader>cp", "<Cmd>CopilotChat<CR>", {} }),
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
 
-		vim.api.nvim_set_keymap("n", "<leader>cp", "<cmd>CopilotChat<CR>", { noremap = true, silent = true }),
-	},
+    build = "make tiktoken",
+    opts = {
+      debug = true, -- Enable debugging
+      question_header = "## Vinod ",
+      auto_follow_cursor = true,
+      highlight_selection = true,
+    },
+    cmd = { "CopilotChat" }, -- Lazy load
+    -- config = function()
+    --   local function start_copilotchat_in_insert_mode()
+    --     vim.cmd("CopilotChat")
+    --     vim.cmd("startinsert") -- Start insert mode
+    --   end
+    -- end,
+
+    vim.api.nvim_set_keymap("n", "<leader>cp", "<Cmd>CopilotChat<CR>", { noremap = true, silent = true }),
+  },
 }
