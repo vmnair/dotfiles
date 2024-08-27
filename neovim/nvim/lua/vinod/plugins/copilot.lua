0eddb202d9e3a82bed426ada5cf653e9a4b6e90e
Vinod Nair <vinod.nair@omshealth.com>
2024-08-27 07:41:52 -0500

    Updated Copilot.lua

--- neovim/nvim/lua/vinod/plugins/copilot.lua
+++ neovim/nvim/lua/vinod/plugins/copilot.lua
@@ -1,9 +1,45 @@
 -- Github Copilot
+vim.g.copilot_no_tab_map = true
 return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   event = "InsertEnter",
+
   config = function()
-    require("copilot").setup({})
+    require("copilot").setup({
+      -- What options are available  here
+
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<Tab>",
+          refresh = "gr",
+          open = "<M-CR>",
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 10,
+        -- keymap = {
+        --   accept = "<M-l>",
+        --   next = "<M-]>",
+        --   prev = "<M-[>",
+        --   dismiss = "<C-]>",
+        -- },
+      },
+      filetypes = {
+        c = true,
+        lua = true,
+        rust = true,
+        go = true,
+        ["."] = false,            -- disable foe all other filetypes
+      },
+      copilot_node_command = "node", -- Node.js version must be > 16.x
+      server_opts_overrides = {},
+    })
   end,
 }
