97425c9891f75bd18076c9dbe099acf930a88b1f
Vinod Nair <vinod.nair@omshealth.com>
2024-09-06 20:31:17 -0500

    Updated wezterm configuration file.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,182 +7,183 @@ local config = {}
 -- Local functions
 -- Show which key table is active in the status area
 wezterm.on("update-right-status", function(window, _)
-  local name = window:active_key_table()
-  if name then
-    name = "TABLE: " .. name
-  end
-  window:set_right_status(name or "")
+	local name = window:active_key_table()
+	if name then
+		name = "TABLE: " .. name
+	end
+	window:set_right_status(name or "")
 end)
 
 -- Leader key for Wezterm <C-a>
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
---
+
+-- Key configuration
 config.keys = {
-  -- Toogle zoom pane
-  {
-    key = "z",
-    mods = "LEADER",
-    action = act.TogglePaneZoomState,
-  },
-  -- Split into panes
-  {
-    key = "|",
-    mods = "LEADER",
-    action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "_",
-    mods = "LEADER",
-    action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-  },
+	-- Toogle zoom pane
+	{
+		key = "z",
+		mods = "LEADER",
+		action = act.TogglePaneZoomState,
+	},
+	-- Split into panes
+	{
+		key = "|",
+		mods = "LEADER",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "_",
+		mods = "LEADER",
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
 
-  -- Switch panes
-  {
-    key = "{",
-    mods = "LEADER",
-    action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }),
-  },
+	-- Switch panes
+	{
+		key = "{",
+		mods = "LEADER",
+		action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }),
+	},
 
-  -- Adjust Pane Size
-  {
-    key = "RightArrow",
-    mods = "LEADER",
-    action = act.AdjustPaneSize({ "Right", 5 }),
-  },
-  {
-    key = "LeftArrow",
-    mods = "LEADER",
-    action = act.AdjustPaneSize({ "Left", 5 }),
-  },
-  {
-    key = "UpArrow",
-    mods = "LEADER",
-    action = act.AdjustPaneSize({ "Up", 5 }),
-  },
-  {
-    key = "DownArrow",
-    mods = "LEADER",
-    action = act.AdjustPaneSize({ "Down", 5 }),
-  },
+	-- Adjust Pane Size
+	{
+		key = "RightArrow",
+		mods = "LEADER",
+		action = act.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		key = "LeftArrow",
+		mods = "LEADER",
+		action = act.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		key = "UpArrow",
+		mods = "LEADER",
+		action = act.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		key = "DownArrow",
+		mods = "LEADER",
+		action = act.AdjustPaneSize({ "Down", 5 }),
+	},
 
-  -- Move betweeen panes
-  {
-    key = "h",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection("Left"),
-  },
-  {
-    key = "l",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection("Right"),
-  },
-  {
-    key = "k",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection("Up"),
-  },
-  {
-    key = "j",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection("Down"),
-  },
+	-- Move betweeen panes
+	{
+		key = "h",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Down"),
+	},
 
-  -- Activate resize table
-  {
-    key = "r",
-    mods = "LEADER",
-    action = act.ActivateKeyTable({
-      name = "resize_pane",
-      one_shot = false,
-    }),
-  },
+	-- Activate resize table
+	{
+		key = "r",
+		mods = "LEADER",
+		action = act.ActivateKeyTable({
+			name = "resize_pane",
+			one_shot = false,
+		}),
+	},
 
-  --Copy mode
-  {
-    key = "[",
-    mods = "LEADER",
-    action = wezterm.action.ActivateCopyMode,
-  },
-  -- Create a new tab
-  {
-    key = "t",
-    mods = "LEADER",
-    action = act.SpawnTab("CurrentPaneDomain"),
-  },
+	--Copy mode
+	{
+		key = "[",
+		mods = "LEADER",
+		action = wezterm.action.ActivateCopyMode,
+	},
+	-- Create a new tab
+	{
+		key = "t",
+		mods = "LEADER",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
 
-  {
-    key = "n",
-    mods = "LEADER",
-    action = wezterm.action.ActivateTabRelative(1),
-  },
+	{
+		key = "n",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
 
-  {
-    key = "p",
-    mods = "LEADER",
-    action = wezterm.action.ActivateTabRelative(-1),
-  },
-  -- Tab Management
-  -- name a tab
-  {
-    key = "e",
-    mods = "LEADER",
-    action = act.PromptInputLine({
-      description = "Tab Name: ",
-      action = wezterm.action_callback(function(window, _, line)
-        if line then
-          window:active_tab():set_title(line)
-        end
-      end),
-    }),
-  },
+	{
+		key = "p",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	-- Tab Management
+	-- name a tab
+	{
+		key = "e", -- Edit tab
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = "Tab Name: ",
+			action = wezterm.action_callback(function(window, _, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 
-  -- Navigate tabs
-  {
-    key = "w",
-    mods = "LEADER",
-    action = act.ShowTabNavigator,
-  },
+	-- Navigate tabs
+	{
+		key = "w",
+		mods = "LEADER",
+		action = act.ShowTabNavigator,
+	},
 
-  -- Close tab
-  {
-    key = "x",
-    mods = "LEADER",
-    action = act.CloseCurrentTab({ confirm = true }),
-  },
+	-- Close tab
+	{
+		key = "x",
+		mods = "LEADER",
+		action = act.CloseCurrentTab({ confirm = true }),
+	},
 }
 
 config.key_tables = {
-  resize_pane = {
-    { key = "LeftArrow",  action = act.AdjustPaneSize({ "Left", 1 }) },
-    { key = "h",          action = act.AdjustPaneSize({ "Left", 1 }) },
+	resize_pane = {
+		{ key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
 
-    { key = "RightArrow", action = act.AdjustPaneSize({ "Right", 1 }) },
-    { key = "l",          action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "RightArrow", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
 
-    { key = "UpArrow",    action = act.AdjustPaneSize({ "Up", 1 }) },
-    { key = "k",          action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "UpArrow", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
 
-    { key = "DownArrow",  action = act.AdjustPaneSize({ "Down", 1 }) },
-    { key = "j",          action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "DownArrow", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
 
-    -- cancel this mode by pressing escape.
-    { key = "Escape",     action = "PopKeyTable" },
-  },
+		-- cancel this mode by pressing escape.
+		{ key = "Escape", action = "PopKeyTable" },
+	},
 }
 
 -- Show tabbar
 config.enable_tab_bar = true
-config.tab_bar_at_bottom = true
+config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 config.switch_to_last_active_tab_when_closing_tab = true
 config.colors = {
-  tab_bar = {
-    background = "#1a1b26",
-    active_tab = {
-      bg_color = "#1a1b26",
-      fg_color = "#7aa2f7",
-    },
-  },
+	tab_bar = {
+		background = "#1a1b26",
+		active_tab = {
+			bg_color = "#1a1b26",
+			fg_color = "#7aa2f7",
+		},
+	},
 }
 config.font = wezterm.font("JetBrains Mono")
 -- config.color_scheme = "Catppuccin Mocha"
