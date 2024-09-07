aecfd077138cf8f5c122ec9b5836539bc1584d40
Vinod Nair <vinod.nair@omshealth.com>
2024-09-07 16:55:46 -0500

    Update wezterm config

--- neovim/nvim/lua/vinod/plugins/nvim-ai.lua
+++
@@ -1,7 +0,0 @@
-return {
-  -- dir = "~/Projects/Neovim/nvim-ai",
-  -- name = "nvim-ai",
-  -- config = function()
-  -- 	require("nvim-ai")
-  -- end,
-}
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,15 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 local act = wezterm.action
+local mux = wezterm.mux
 -- This will hold the configuration.
 -- local config = wezterm.config_builder()
 local config = {}
+
+-- use config_builder if it is available
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
 -- Local functions
 -- Show which key table is active in the status area
 wezterm.on("update-right-status", function(window, _)
@@ -16,7 +22,39 @@ end)
 
 -- Leader key for Wezterm <C-a>
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+config.window_background_opacity = 0.9
+config.window_decorations = "RESIZE"
+config.default_workspace = "home"
 
+-- Show tabbar
+config.enable_tab_bar = true
+config.tab_bar_at_bottom = false -- default is top
+config.use_fancy_tab_bar = false
+config.tab_max_width = 32
+config.switch_to_last_active_tab_when_closing_tab = true
+config.pane_focus_follows_mouse = true
+config.scrollback_lines = 5000
+
+--  No padding between panes
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.colors = {
+	tab_bar = {
+		background = "#1a1b26",
+		active_tab = {
+			bg_color = "#1a1b26",
+			fg_color = "#7aa2f7",
+		},
+	},
+}
+config.font = wezterm.font("JetBrains Mono")
+-- config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Tokyo Night Storm"
+config.font_size = 16
 -- Key configuration
 config.keys = {
 	-- Toogle zoom pane
@@ -150,6 +188,38 @@ config.keys = {
 		mods = "LEADER",
 		action = act.CloseCurrentTab({ confirm = true }),
 	},
+
+	-- Mux server connection
+	{
+		key = "a",
+		mods = "LEADER",
+		action = act.AttachDomain("unix"),
+	},
+
+	{
+		key = "d",
+		mods = "LEADER",
+		action = act.DetachDomain({ DomainName = "unix" }),
+	},
+	-- rename current session
+	{
+		key = "$",
+		mods = "LEADER|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for session",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					mux.rename_workspace(window:mux_window():get_workspace(), line)
+				end
+			end),
+		}),
+	},
+	-- Show list of workspaces
+	{
+		key = "s",
+		mods = "LEADER",
+		action = act.ShowLauncherArgs({ flags = "WORKSPACES" }),
+	},
 }
 
 config.key_tables = {
@@ -171,23 +241,11 @@ config.key_tables = {
 	},
 }
 
--- Show tabbar
-config.enable_tab_bar = true
-config.tab_bar_at_bottom = false
-config.use_fancy_tab_bar = false
-config.switch_to_last_active_tab_when_closing_tab = true
-config.colors = {
-	tab_bar = {
-		background = "#1a1b26",
-		active_tab = {
-			bg_color = "#1a1b26",
-			fg_color = "#7aa2f7",
-		},
+-- Mux server configuration
+config.unix_domains = {
+	{
+		name = "unix",
 	},
 }
-config.font = wezterm.font("JetBrains Mono")
--- config.color_scheme = "Catppuccin Mocha"
-config.color_scheme = "Tokyo Night Storm"
-config.font_size = 16
 
 return config
