908928e61057bbbf1f9a214e28162390c62796d9
Vinod Nair <vinod.nair@omshealth.com>
2024-12-29 07:20:29 +0530

    updted

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 local act = wezterm.action
-local mux = wezterm.mux
--- This will hold the configuration.
--- local config = wezterm.config_builder()
 local config = {}
 
 -- maximize on startup
@@ -13,9 +10,10 @@ local config = {}
 -- end)
 
 -- use config_builder if it is available
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+-- if wezterm.config_builder then
+--   config = wezterm.config_builder()
+-- end
+
 -- Local functions
 -- Show which key table is active in the status area
 wezterm.on("update-right-status", function(window, _)
@@ -47,6 +45,7 @@ config.window_padding = {
   top = 0,
   bottom = 0,
 }
+
 config.colors = {
   tab_bar = {
     background = "#1a1b26",
@@ -67,178 +66,178 @@ config.font_size = 16
 config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 2000 }
 
 -- Key configuration
-config.keys = {
-  -- Toogle zoom pane
-  {
-    key = "z",
-    mods = "LEADER",
-    action = act.TogglePaneZoomState,
-  },
-  -- Split into panes
-  {
-    key = "|",
-    mods = "LEADER",
-    action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "_",
-    mods = "LEADER",
-    action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-  },
-  -- Switch panes
-  {
-    key = "{",
-    mods = "LEADER",
-    action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }),
-  },
-  -- Adjust Pane Size
-  {
-    key = "RightArrow",
-    mods = "LEADER",
-    action = act.AdjustPaneSize({ "Right", 5 }),
-  },
-  {
-    key = "LeftArrow",
-    mods = "LEADER",
-    action = act.AdjustPaneSize({ "Left", 5 }),
-  },
-  {
-    key = "UpArrow",
-    mods = "LEADER",
-    action = act.AdjustPaneSize({ "Up", 5 }),
-  },
-  {
-    key = "DownArrow",
-    mods = "LEADER",
-    action = act.AdjustPaneSize({ "Down", 5 }),
-  },
-  -- Move betweeen panes
-  {
-    key = "h",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection("Left"),
-  },
-  {
-    key = "l",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection("Right"),
-  },
-  {
-    key = "k",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection("Up"),
-  },
-  {
-    key = "j",
-    mods = "LEADER",
-    action = act.ActivatePaneDirection("Down"),
-  },
-  -- Activate resize table
-  {
-    key = "r",
-    mods = "LEADER",
-    action = act.ActivateKeyTable({
-      name = "resize_pane",
-      one_shot = false,
-    }),
-  },
-  --Copy mode
-  {
-    key = "[",
-    mods = "LEADER",
-    action = wezterm.action.ActivateCopyMode,
-  },
-  -- Create a new tab
-  {
-    key = "t",
-    mods = "LEADER",
-    action = act.SpawnTab("CurrentPaneDomain"),
-  },
+-- config.keys = {
+--   -- Toogle zoom pane
+--   {
+--     key = "z",
+--     mods = "LEADER",
+--     action = act.TogglePaneZoomState,
+--   },
+--   -- Split into panes
+--   {
+--     key = "|",
+--     mods = "LEADER",
+--     action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+--   },
+--   {
+--     key = "_",
+--     mods = "LEADER",
+--     action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+--   },
+--   -- Switch panes
+--   {
+--     key = "{",
+--     mods = "LEADER",
+--     action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }),
+--   },
+--   -- Adjust Pane Size
+--   {
+--     key = "RightArrow",
+--     mods = "LEADER",
+--     action = act.AdjustPaneSize({ "Right", 5 }),
+--   },
+--   {
+--     key = "LeftArrow",
+--     mods = "LEADER",
+--     action = act.AdjustPaneSize({ "Left", 5 }),
+--   },
+--   {
+--     key = "UpArrow",
+--     mods = "LEADER",
+--     action = act.AdjustPaneSize({ "Up", 5 }),
+--   },
+--   {
+--     key = "DownArrow",
+--     mods = "LEADER",
+--     action = act.AdjustPaneSize({ "Down", 5 }),
+--   },
+--   -- Move betweeen panes
+--   {
+--     key = "h",
+--     mods = "LEADER",
+--     action = act.ActivatePaneDirection("Left"),
+--   },
+--   {
+--     key = "l",
+--     mods = "LEADER",
+--     action = act.ActivatePaneDirection("Right"),
+--   },
+--   {
+--     key = "k",
+--     mods = "LEADER",
+--     action = act.ActivatePaneDirection("Up"),
+--   },
+--   {
+--     key = "j",
+--     mods = "LEADER",
+--     action = act.ActivatePaneDirection("Down"),
+--   },
+--   -- Activate resize table
+--   {
+--     key = "r",
+--     mods = "LEADER",
+--     action = act.ActivateKeyTable({
+--       name = "resize_pane",
+--       one_shot = false,
+--     }),
+--   },
+--   --Copy mode
+--   {
+--     key = "[",
+--     mods = "LEADER",
+--     action = wezterm.action.ActivateCopyMode,
+--   },
+--   -- Create a new tab
+--   {
+--     key = "t",
+--     mods = "LEADER",
+--     action = act.SpawnTab("CurrentPaneDomain"),
+--   },
+--
+--   {
+--     key = "n",
+--     mods = "LEADER",
+--     action = wezterm.action.ActivateTabRelative(1),
+--   },
+--
+--   {
+--     key = "p",
+--     mods = "LEADER",
+--     action = wezterm.action.ActivateTabRelative(-1),
+--   },
+--   -- Tab Management
+--   -- name a tab
+--   {
+--     key = "e", -- Edit tab
+--     mods = "LEADER",
+--     action = act.PromptInputLine({
+--       description = "Tab Name: ",
+--       action = wezterm.action_callback(function(window, _, line)
+--         if line then
+--           window:active_tab():set_title(line)
+--         end
+--       end),
+--     }),
+--   },
+--   -- Navigate tabs
+--   {
+--     key = "w",
+--     mods = "LEADER",
+--     action = act.ShowTabNavigator,
+--   },
+--   -- Close tab
+--   {
+--     key = "x",
+--     mods = "LEADER",
+--     action = act.CloseCurrentTab({ confirm = true }),
+--   },
+--   -- Mux server connection
+--   {
+--     key = "a",
+--     mods = "LEADER",
+--     action = act.AttachDomain("unix"),
+--   },
+--
+--   {
+--     key = "d",
+--     mods = "LEADER",
+--     action = act.DetachDomain({ DomainName = "unix" }),
+--   },
+--   -- Toggle pane zoom state
+--   {
+--     key = "f",
+--     mods = "LEADER",
+--     -- mods = "ALT",
+--     action = wezterm.action.TogglePaneZoomState,
+--   },
+-- }
 
-  {
-    key = "n",
-    mods = "LEADER",
-    action = wezterm.action.ActivateTabRelative(1),
-  },
-
-  {
-    key = "p",
-    mods = "LEADER",
-    action = wezterm.action.ActivateTabRelative(-1),
-  },
-  -- Tab Management
-  -- name a tab
-  {
-    key = "e", -- Edit tab
-    mods = "LEADER",
-    action = act.PromptInputLine({
-      description = "Tab Name: ",
-      action = wezterm.action_callback(function(window, _, line)
-        if line then
-          window:active_tab():set_title(line)
-        end
-      end),
-    }),
-  },
-  -- Navigate tabs
-  {
-    key = "w",
-    mods = "LEADER",
-    action = act.ShowTabNavigator,
-  },
-  -- Close tab
-  {
-    key = "x",
-    mods = "LEADER",
-    action = act.CloseCurrentTab({ confirm = true }),
-  },
-  -- Mux server connection
-  {
-    key = "a",
-    mods = "LEADER",
-    action = act.AttachDomain("unix"),
-  },
-
-  {
-    key = "d",
-    mods = "LEADER",
-    action = act.DetachDomain({ DomainName = "unix" }),
-  },
-  -- Toggle pane zoom state
-  {
-    key = "f",
-    mods = "LEADER",
-    -- mods = "ALT",
-    action = wezterm.action.TogglePaneZoomState,
-  },
-}
-
-config.key_tables = {
-  resize_pane = {
-    { key = "LeftArrow",  action = act.AdjustPaneSize({ "Left", 1 }) },
-    { key = "h",          action = act.AdjustPaneSize({ "Left", 1 }) },
-
-    { key = "RightArrow", action = act.AdjustPaneSize({ "Right", 1 }) },
-    { key = "l",          action = act.AdjustPaneSize({ "Right", 1 }) },
-
-    { key = "UpArrow",    action = act.AdjustPaneSize({ "Up", 1 }) },
-    { key = "k",          action = act.AdjustPaneSize({ "Up", 1 }) },
-
-    { key = "DownArrow",  action = act.AdjustPaneSize({ "Down", 1 }) },
-    { key = "j",          action = act.AdjustPaneSize({ "Down", 1 }) },
-
-    -- cancel this mode by pressing escape.
-    { key = "Escape",     action = "PopKeyTable" },
-  },
-}
+-- config.key_tables = {
+--   resize_pane = {
+--     { key = "LeftArrow",  action = act.AdjustPaneSize({ "Left", 1 }) },
+--     { key = "h",          action = act.AdjustPaneSize({ "Left", 1 }) },
+--
+--     { key = "RightArrow", action = act.AdjustPaneSize({ "Right", 1 }) },
+--     { key = "l",          action = act.AdjustPaneSize({ "Right", 1 }) },
+--
+--     { key = "UpArrow",    action = act.AdjustPaneSize({ "Up", 1 }) },
+--     { key = "k",          action = act.AdjustPaneSize({ "Up", 1 }) },
+--
+--     { key = "DownArrow",  action = act.AdjustPaneSize({ "Down", 1 }) },
+--     { key = "j",          action = act.AdjustPaneSize({ "Down", 1 }) },
+--
+--     -- cancel this mode by pressing escape.
+--     { key = "Escape",     action = "PopKeyTable" },
+--   },
+-- }
 
 -- We can use the tab navigator (LDR t), but I also want to quickly navigate tabs with index
-for i = 1, 9 do
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = "LEADER",
-    action = wezterm.action.ActivateTab(i - 1),
-  })
-end
+-- for i = 1, 9 do
+--   table.insert(config.keys, {
+--     key = tostring(i),
+--     mods = "LEADER",
+--     action = wezterm.action.ActivateTab(i - 1),
+--   })
+-- end
 
 return config
