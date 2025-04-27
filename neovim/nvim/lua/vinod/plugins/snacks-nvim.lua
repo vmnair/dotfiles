d39c0f12e07e4ac3288193b2d2bace7e9c797ccc
Vinod Nair <vinod.nair@omshealth.com>
2025-04-27 01:03:02 -0500

    Added snacks explorer and deleted nvim-tree

--- neovim/nvim/lua/vinod/plugins/snacks-nvim.lua
+++ neovim/nvim/lua/vinod/plugins/snacks-nvim.lua
@@ -19,15 +19,27 @@ return {
         pick = nil,
       },
     },
-    explorer = { enabled = true },
-    -- indent = { enabled = true },
-    -- input = { enabled = true },
-    picker = { enabled = true },
-    notifier = { enabled = true },
-    -- quickfile = { enabled = true },
-    -- scope = { enabled = true },
-    -- scroll = { enabled = true },
-    -- statuscolumn = { enabled = true },
-    -- words = { enabled = true },
+    explorer = {
+      enabled = true,
+      replace_netrw = true,
+    },
+  },
+  -- indent = { enabled = true },
+  -- input = { enabled = true },
+  picker = { enabled = true },
+  notifier = { enabled = true },
+  -- quickfile = { enabled = true },
+  -- scope = { enabled = true },
+  -- scroll = { enabled = true },
+  -- statuscolumn = { enabled = true },
+  -- words = { enabled = true },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("snacks").explorer()
+      end,
+      desc = "Snacks Explorer",
+    },
   },
 }
