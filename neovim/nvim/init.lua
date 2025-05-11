8565127128814baf83667d8599978dbe4b99f7f1
Vinod Nair <vinod.nair@omshealth.com>
2025-05-10 23:46:44 -0500

    Updated lsp.proj & zshrc_mac

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -7,21 +7,3 @@ require("vinod.config.util")
 require("vinod.config.autocmds")
 require("vinod.config.aliases")
 -- require("vinod.config.mappings") Called from lazy.lua
-
---
--- Enable [[ to trigger the completion menu in markdown files
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "markdown",
---   callback = function()
---     if vim.lsp.omnifunc then
---       -- Set the omnifunc to the LSP's omnifunc
---       vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
---       -- Create a buffer-local mapping for [[ to trigger completion
---       vim.api.nvim_buf_set_keymap(0, "i", "[[", "[[<C-x><C-o>", { noremap = true, silent = true })
---     else
---       vim.notify("LSP omnifunc not available", vim.log.levels.WARN)
---     end
---     -- assign the omnifunc of  lsp to local buffer (markdown filetype)
---     -- vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
---   end,
--- })
